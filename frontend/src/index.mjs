339e4bfcc0998a8622295bba46a47de2c51a688b
soundyogi <3201046+soundyogi@users.noreply.github.com>
2023-06-06 23:15:59 +0200

    Create index.mjs

---
+++ frontend/src/index.mjs
@@ -0,0 +1,9 @@
+/* global Phaser, EventEmitter3 */
+import { GAME_WIDTH, GAME_HEIGHT } from './constants.mjs';
+
+
+window.DEBUG = true
+
+
+// global pubsub
+window.RADIO = new window.EventEmitter3()
