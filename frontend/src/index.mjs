c9629cf021b34ca8b48628c0f7aed9761f6c3bed
soundyogi <soundyogi@gmail.com>
2023-06-07 14:58:59 +0200

    add plain css - ITCSS paradigm

--- frontend/src/index.mjs
+++ frontend/src/index.mjs
@@ -1,7 +1,12 @@
 import { ethers } from 'ethers';
+import { EventEmitter } from './util.mjs';
 
-// window.DEBUG = true
-// window.RADIO = new window.EventEmitter3()
+// Global Event Bus optional but useful with minimal approaches
+//
+window.RADIO = new EventEmitter()
+
+window.DEBUG = true
 
 
-// init
+// Setup
+//
