fcfb4a78a9e445aabb36e8f9e3205a4b4cd2dbd6
soundyogi <soundyogi@gmail.com>
2023-06-07 22:55:52 +0200

    POC: ethers connects to Metamask and updates store

--- frontend/src/index.mjs
+++ frontend/src/index.mjs
@@ -1,12 +1,28 @@
-import { ethers } from 'ethers';
+import * as ethers from '../node_modules/ethers/dist/ethers.js';
 import { EventEmitter } from './util.mjs';
+import { walletStore } from './store.mjs';
+
+
+window.DEBUG = true
 
 // Global Event Bus optional but useful with minimal approaches
 //
 window.RADIO = new EventEmitter()
 
-window.DEBUG = true
-
 
 // Setup
 //
+
+
+// Input Handlers
+//
+
+document.addEventListener('click', async function (event) {
+    // console.log(event.target)
+	event.preventDefault();
+
+    if(event.target.matches('[data-action="connectWallet"]')){
+        await walletStore.getState().connectWallet()
+    }
+
+}, false);
