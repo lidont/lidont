96a011caab62205ac417aa1cca6de4a18c61d3c4
soundyogi <soundyogi@gmail.com>
2023-06-11 22:33:23 +0200

    cleanup

--- frontend/src/components/_example.mjs
+++ frontend/src/components/_example.mjs
@@ -1,54 +1,36 @@
-const template = document.createElement('template');
-template.innerHTML = `
-  <style>
-    * {
-      font-size: 200%;
-    }
-
-    span {
-      width: 4rem;
-      display: inline-block;
-      text-align: center;
-    }
-
-    button {
-      width: 4rem;
-      height: 4rem;
-      border: none;
-      border-radius: 10px;
-      background-color: seagreen;
-      color: white;
-    }
-  </style>
-  <button id="dec">-</button>
-  <span id="count"></span>
-  <button id="inc">+</button>`;
-
 class Component extends HTMLElement {
   constructor() {
     super();
     this.count = 0;
-    this.attachShadow({ mode: 'open' });
   }
 
   connectedCallback() {
-    this.shadowRoot.appendChild(template.content.cloneNode(true));
-    this.shadowRoot.getElementById('inc').onclick = () => this.inc();
-    this.shadowRoot.getElementById('dec').onclick = () => this.dec();
-    this.update(this.count);
+    this.getElementById('inc').onclick = () => this.inc();
+    this.getElementById('dec').onclick = () => this.dec();
+    this.render(this.count);
   }
 
   inc() {
-    this.update(++this.count);
+    this.render(++this.count);
   }
 
   dec() {
-    this.update(--this.count);
+    this.render(--this.count);
   }
 
-  update(count) {
-    this.shadowRoot.getElementById('count').innerHTML = count;
+  render(count){
+    this.innerHTML = `
+      <div>
+        <button id="inc">inc</button>
+        <button id="dec">inc</button>
+        ${count}
+      </div>
+    `;
   }
+
+  connectedCallback() { this.render(); }
+
+  attributeChangedCallback() { this.render(); }
 }
 
 customElements.define('counter', Component);
