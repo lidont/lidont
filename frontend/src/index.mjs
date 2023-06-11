d6193a86dc387811e21a2d599dd26be3057e8b71
soundyogi <soundyogi@gmail.com>
2023-06-11 07:39:39 +0200

    fixes, added spinner, cleanup, styling

--- frontend/src/index.mjs
+++ frontend/src/index.mjs
@@ -12,6 +12,28 @@ if(window.DEBUG){
 
 
 
+(function () {
+    var angle = 0;
+    var p = document.querySelector('h2');
+    var text = p.textContent.split('');
+    var len = text.length;
+    var phaseJump = 360 / len;
+    var spans;
+  
+    p.innerHTML = text.map(function (char) {
+      return '<span class="heading-rainbow">' + char + '</span>';
+    }).join('');
+  
+    spans = p.children;
+  
+    (function wheee () {
+      for (var i = 0; i < len; i++) {
+        spans[i].style.color = 'hsl(' + (angle + Math.floor(i * phaseJump)) + ', 55%, 70%)';
+      }
+      angle++;
+      requestAnimationFrame(wheee);
+    })();
+  })();
 
 
 
