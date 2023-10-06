39723c94ad1045638430ba9a00b1813dd5f931bf
unforkableco <103948226+unforkableco@users.noreply.github.com>
2023-10-06 14:14:44 +0000

    try to understand finalization better

--- tests/test_lidont.py
+++ tests/test_lidont.py
@@ -81,8 +81,8 @@ def test_toggle_pipe_makes_valid(withdrawler, ETH_pipe, accounts):
 
 @pytest.fixture(scope="function")
 def ETH_pipe_added(withdrawler, ETH_pipe, accounts):
-    receipt = withdrawler.toggleValidOutput(ETH_pipe.address, sender=accounts[0])
-    return {'pipe': ETH_pipe, 'toggle_valid_receipt': receipt}
+    withdrawler.toggleValidOutput(ETH_pipe.address, sender=accounts[0])
+    return ETH_pipe
 
 @pytest.fixture(scope="function")
 def rETH_pipe_added(withdrawler, rETH_pipe, accounts):
@@ -91,7 +91,7 @@ def rETH_pipe_added(withdrawler, rETH_pipe, accounts):
 
 def test_cannot_deposit_no_amount(withdrawler, ETH_pipe_added, accounts):
     with reverts("deposit too small"):
-        withdrawler.deposit(0, ETH_pipe_added['pipe'].address, sender=accounts[0])
+        withdrawler.deposit(0, ETH_pipe_added.address, sender=accounts[0])
 
 @pytest.fixture(scope="function")
 def have_stETH(stETH, accounts):
@@ -99,19 +99,19 @@ def have_stETH(stETH, accounts):
 
 def test_cannot_deposit_not_approved(withdrawler, have_stETH, ETH_pipe_added, accounts):
     with reverts("ALLOWANCE_EXCEEDED"):
-        withdrawler.deposit(100, ETH_pipe_added['pipe'].address, sender=accounts[0])
+        withdrawler.deposit(100, ETH_pipe_added.address, sender=accounts[0])
 
 def test_cannot_deposit_no_balance(withdrawler, stETH, ETH_pipe_added, accounts):
     assert stETH.balanceOf(accounts[0]) == 0
     assert stETH.approve(withdrawler.address, 100, sender=accounts[0])
     with reverts("balance"):
-        withdrawler.deposit(100, ETH_pipe_added['pipe'].address, sender=accounts[0])
+        withdrawler.deposit(100, ETH_pipe_added.address, sender=accounts[0])
 
 @pytest.fixture(scope="function")
 def deposit_ETH_pipe(accounts, withdrawler, have_stETH, stETH, ETH_pipe_added):
     amount = 42 * 10 ** 17
     assert stETH.approve(withdrawler.address, amount, sender=accounts[0])
-    withdrawler.deposit(amount, ETH_pipe_added['pipe'].address, sender=accounts[0])
+    withdrawler.deposit(amount, ETH_pipe_added.address, sender=accounts[0])
     return {"amount": amount}
 
 def test_deposit_pipe_ETH(withdrawler, deposit_ETH_pipe, accounts):
@@ -160,11 +160,13 @@ def one_withdrawal_finalized(withdrawler, addr, stETH, unstETH, one_withdrawal_i
     attempts = 0
     while unstETH.getLastFinalizedRequestId() < requestId:
         # stake more ETH with Lido to "increase buffered ETH in the protocol"
+        stakingLimit = stETH.getCurrentStakeLimit()
         stETH.submit(accounts[2], value='1024 ETH', sender=accounts[2])
         chain.mine(256)
         stETH.submit(accounts[3], value='1024 ETH', sender=accounts[3])
         chain.mine(256, None, ONE_DAY_SECONDS)
-        stETH.submit(accounts[4], value='1024 ETH', sender=accounts[4])
+        accounts[4].balance += int(1e24) # give 1000000 ETH
+        stETH.submit(accounts[4], value=stakingLimit, sender=accounts[4])
         chain.mine(256, None, 2 * ONE_DAY_SECONDS)
 
         refSlot = HashConsensusContract.getCurrentFrame()[0]
@@ -210,6 +212,7 @@ def one_withdrawal_finalized(withdrawler, addr, stETH, unstETH, one_withdrawal_i
                     simulatedShareRate, maxTimestamp, MAX_REQUESTS_PER_CALL, batchesState
                 )
         withdrawalFinalizationBatches = list(filter(lambda value: value > 0, batchesState[2]))
+        print(withdrawalFinalizationBatches)
         preTotalPooledEther = stETH.getTotalPooledEther()
         is_bunker = preTotalPooledEther > postTotalPooledEther
         consensusVersion = AccountingOracleContract.getConsensusVersion()
@@ -258,11 +261,19 @@ def one_withdrawal_claimed(one_withdrawal_finalized, withdrawler, accounts):
 
 def test_claim(one_withdrawal_claimed, ETH_pipe_added, deposit_ETH_pipe, accounts):
     assert one_withdrawal_claimed.return_value == deposit_ETH_pipe["amount"]
-    assert ETH_pipe_added['pipe'].stakes(accounts[0]).amount == one_withdrawal_claimed.return_value
-    logs = ETH_pipe_added['pipe'].Stake.from_receipt(one_withdrawal_claimed)
+    assert ETH_pipe_added.stakes(accounts[0]).amount == one_withdrawal_claimed.return_value
+    logs = ETH_pipe_added.Stake.from_receipt(one_withdrawal_claimed)
     assert len(logs) == 1
     assert logs[0].user == accounts[0]
     assert logs[0].amount == one_withdrawal_claimed.return_value
+
+def test_block_number(withdrawler, chain, accounts):
+    receipt = withdrawler.testBlockNum(sender=accounts[0])
+    assert receipt.block_number == withdrawler.TestBlockNum.from_receipt(receipt)[0].blocknum
+    chain.mine(3)
+    receipt2 = withdrawler.testBlockNum(sender=accounts[0])
+    chain.mine(4)
+    assert receipt2.block_number == withdrawler.TestBlockNum.from_receipt(receipt2)[0].blocknum
 
 def test_unstake_partial(lidont, withdrawler, start_emission, ETH_pipe_added, one_withdrawal_claimed, chain, accounts):
     stake_blocks = ONE_DAY_SECONDS // 12 + 128
@@ -271,15 +282,16 @@ def test_unstake_partial(lidont, withdrawler, start_emission, ETH_pipe_added, on
     after_mine = chain.blocks.head.number
     assert after_mine - before_mine == stake_blocks
     setLastLogs = list(withdrawler.SetLastRewardBlock.range(withdrawler.receipt.block_number, chain.blocks.head.number))
-    assert setLastLogs[-1].bnum == ETH_pipe_added['toggle_valid_receipt'].block_number
-    emission_receipt = withdrawler.triggerEmission(ETH_pipe_added['pipe'].address, sender=accounts[1]) # TODO: should this be automatic in some contract?
+    assert setLastLogs[-1].bnum == start_emission.block_number
+    assert len(setLastLogs) == 2
+    emission_receipt = withdrawler.triggerEmission(ETH_pipe_added.address, sender=accounts[1]) # TODO: should this be automatic in some contract?
     mint_logs = lidont.Mint.from_receipt(emission_receipt)
-    assert len(ETH_pipe_added['pipe'].Receive.from_receipt(emission_receipt)) == 1
+    assert len(ETH_pipe_added.Receive.from_receipt(emission_receipt)) == 1
     assert len(mint_logs) == 1
-    assert mint_logs[0].recipient == ETH_pipe_added['pipe'].address
-    assert mint_logs[0].amount == (emission_receipt.block_number - ETH_pipe_added['toggle_valid_receipt'].block_number) * EMISSION_PER_BLOCK
+    assert mint_logs[0].recipient == ETH_pipe_added.address
+    assert mint_logs[0].amount == (emission_receipt.block_number - start_emission.block_number) * EMISSION_PER_BLOCK
     amount = one_withdrawal_claimed.return_value // 2
-    receipt = ETH_pipe_added['pipe'].unstake(amount, sender=accounts[0])
-    logs = ETH_pipe_added['pipe'].Unstake.from_receipt(receipt)
+    receipt = ETH_pipe_added.unstake(amount, sender=accounts[0])
+    logs = ETH_pipe_added.Unstake.from_receipt(receipt)
     assert len(logs) == 1
     assert lidont.balanceOf(accounts[0]) == 39 # TODO: calculate correctly
