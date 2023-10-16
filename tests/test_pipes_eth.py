fe7e9c9679f3408bc98c9d1985739f7096b96960
unforkableco <103948226+unforkableco@users.noreply.github.com>
2023-10-16 14:19:46 +0000

    fixed broken eth pipe tests

--- tests/test_pipes_eth.py
+++ tests/test_pipes_eth.py
@@ -10,15 +10,12 @@ addresses = dict(mainnet =
                       ),
                  )
 
+EMISSION_PER_BLOCK = 10 ** 9
+
 @pytest.fixture(scope="session")
 def addr(networks):
     network = networks.provider.network.name.removesuffix('-fork')
     return addresses[network]
-
-@pytest.fixture(scope="session")
-def lidont(project, accounts):
-    lidont = project.lidont.deploy(accounts[0].address, sender=accounts[0])
-    return lidont
 
 @pytest.fixture(scope="session")
 def withdrawler(project, addr, accounts):
@@ -27,12 +24,24 @@ def withdrawler(project, addr, accounts):
     return withdrawler
 
 @pytest.fixture(scope="session")
+def start_emission(withdrawler, accounts):
+    return withdrawler.changeEmissionRate(EMISSION_PER_BLOCK, sender=accounts[0])
+
+@pytest.fixture(scope="session")
+def lidont(project, accounts, withdrawler):
+    lidont = project.lidont.deploy(withdrawler.address, sender=accounts[0])
+    withdrawler.setLidont(lidont.address, sender=accounts[0])
+    return lidont
+
+@pytest.fixture(scope="session")
 def ETH_pipe(project, lidont, withdrawler, accounts):
-    return project._get_attr('ETH-pipe').deploy(lidont.address, withdrawler.address, sender=accounts[0])
+    pipe = project._get_attr('ETH-pipe').deploy(lidont.address, withdrawler.address, sender=accounts[0])
+    withdrawler.toggleValidOutput(pipe.address, sender=accounts[0])
+    return pipe
 
 @pytest.fixture(scope="function")
-def mint_lidont(lidont, project, accounts):
-    lidont.mint(800 * 10 ** 18, accounts[0].address, sender=accounts[0])
+def mint_lidont(lidont, project, withdrawler, accounts):
+    lidont.mint(800 * 10 ** 18, accounts[0].address, sender=withdrawler)
     lidont.transferFrom("0x0000000000000000000000000000000000000000", accounts[0], 800 * 10 ** 18, sender=accounts[0])
     return lidont.balanceOf(accounts[0].address)
 
@@ -81,20 +90,20 @@ def test_rewards_eth(ETH_pipe, lidont, mint_lidont, setup_allowance, stake_ETH, 
     assert ETH_pipe.bondValue() == 100000000 + bondInc
     assert ETH_pipe.dust() == distribute_reward - totalDistributed
 
-def test_rewards_eth_temp_clearing(ETH_pipe, lidont, mint_lidont, setup_allowance, distribute_reward, stake_ETH, accounts):
+def test_rewards_eth_temp_clearing(ETH_pipe, lidont, withdrawler, mint_lidont, setup_allowance, distribute_reward, stake_ETH, accounts):
     assert ETH_pipe.temp() == mint_lidont
-    lidont.mint(800 * 10 ** 18, accounts[0].address, sender=accounts[0])
+    lidont.mint(800 * 10 ** 18, accounts[0].address, sender=withdrawler)
     lidont.transferFrom("0x0000000000000000000000000000000000000000", accounts[0], 800 * 10 ** 18, sender=accounts[0])
     ETH_pipe.receiveReward(lidont.address, accounts[0].address, mint_lidont, sender=accounts[0])
     assert ETH_pipe.temp() == 0
 
-def test_rewards_eth_dust_clearing(ETH_pipe, lidont, mint_lidont, setup_allowance, stake_ETH, distribute_reward, accounts):
+def test_rewards_eth_dust_clearing(ETH_pipe, lidont, withdrawler, mint_lidont, setup_allowance, stake_ETH, distribute_reward, accounts):
     assert ETH_pipe.dust() > 0
     dust = ETH_pipe.dust()
     totalBonds = ETH_pipe.totalStake() // 10 ** 9
     perfectReward = totalBonds * 2
     rewardsToClear = perfectReward - dust
-    lidont.mint(rewardsToClear, accounts[0].address, sender=accounts[0])
+    lidont.mint(rewardsToClear, accounts[0].address, sender=withdrawler)
     lidont.transferFrom("0x0000000000000000000000000000000000000000", accounts[0], rewardsToClear, sender=accounts[0])
     ETH_pipe.receiveReward(lidont.address, accounts[0].address, rewardsToClear, sender=accounts[0])
     assert ETH_pipe.dust() == 0
